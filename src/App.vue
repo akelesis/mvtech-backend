efbe0012da937ce247956522260eeddbc297b97e
brenu <bvsousa64@gmail.com>
2021-01-30 22:02:25 -0300

    :rocket: Create section component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Section :color="'#101838'">
+      <img :src="companyLogo" alt="Logo da empresa" id="mvtech-logo" />
+    </Section>
+    <Menu />
+    <Section></Section>
+    <Section></Section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Menu from "./components/Menu.vue";
+import Section from "./components/Section.vue";
+import companyLogo from "./assets/images/mvtech.png";
 
 export default {
-  name: 'App',
+  name: "App",
+  data() {
+    return {
+      companyLogo,
+    };
+  },
   components: {
-    HelloWorld
-  }
-}
+    Menu,
+    Section,
+  },
+};
 </script>
 
 <style>
+*,
+body {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+
+#mvtech-logo {
+  width: 60vw;
 }
 </style>
---
+++ src/components/Section.vue
@@ -0,0 +1,32 @@
+<template>
+  <section :style="{ 'background-color': color }">
+    <slot />
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Section",
+  data() {
+    return {};
+  },
+  props: {
+    color: {
+      type: String,
+      required: false,
+      default: "#3E6A94",
+    },
+  },
+};
+</script>
+
+<style scoped>
+section {
+  height: calc(100vh - 62px);
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+}
+</style>
