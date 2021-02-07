c0b8d1a40578d7ff219b91601b15c547d636806e
brenu <bvsousa64@gmail.com>
2021-02-07 10:51:04 -0300

    :lock: Add navbar test cases for different viewports

--- cypress/integration/unit/navbar.spec.js
+++ cypress/integration/unit/navbar.spec.js
@@ -21,9 +21,115 @@ describe("Navbar Test", () => {
     cy.contains("Solicite Orçamento").click();
     cy.url().should("include", "#budget");
     cy.get("#budget").isInViewport();
+
+    cy.get("button").should("not.be.visible");
   });
 
-  it("Mobile device", () => {
+  it("Mobile device (smartphone)", () => {
+    cy.viewport("iphone-x");
     cy.visit("http://localhost:8080");
+
+    cy.get("button").click();
+    cy.get(".mobile-navigator")
+      .contains("Quem Somos")
+      .click();
+    cy.url().should("include", "#about");
+    cy.get("#about").isInViewport();
+
+    cy.get("button").click();
+    cy.get(".mobile-navigator")
+      .contains("Serviços")
+      .click();
+    cy.url().should("include", "#services");
+    cy.get("#services").isInViewport();
+
+    cy.get("button").click();
+    cy.get(".mobile-navigator")
+      .contains("Tecnologias")
+      .click();
+    cy.url().should("include", "#tecnologies");
+    cy.get("#tecnologies").isInViewport();
+
+    cy.get("button").click();
+    cy.get(".mobile-navigator")
+      .contains("Portfólio")
+      .click();
+    cy.url().should("include", "#portfolio");
+    cy.get("#portfolio").isInViewport();
+
+    cy.get("button").click();
+    cy.get(".mobile-navigator")
+      .contains("Solicite Orçamento")
+      .click();
+    cy.url().should("include", "#budget");
+    cy.get("#budget").isInViewport();
+  });
+
+  it("Medium tablet", () => {
+    cy.viewport("ipad-2");
+    cy.visit("http://localhost:8080");
+
+    cy.get("button").click();
+    cy.get(".mobile-navigator")
+      .contains("Quem Somos")
+      .click();
+    cy.url().should("include", "#about");
+    cy.get("#about").isInViewport();
+
+    cy.get("button").click();
+    cy.get(".mobile-navigator")
+      .contains("Serviços")
+      .click();
+    cy.url().should("include", "#services");
+    cy.get("#services").isInViewport();
+
+    cy.get("button").click();
+    cy.get(".mobile-navigator")
+      .contains("Tecnologias")
+      .click();
+    cy.url().should("include", "#tecnologies");
+    cy.get("#tecnologies").isInViewport();
+
+    cy.get("button").click();
+    cy.get(".mobile-navigator")
+      .contains("Portfólio")
+      .click();
+    cy.url().should("include", "#portfolio");
+    cy.get("#portfolio").isInViewport();
+
+    cy.get("button").click();
+    cy.get(".mobile-navigator")
+      .contains("Solicite Orçamento")
+      .click();
+    cy.url().should("include", "#budget");
+    cy.get("#budget").isInViewport();
+  });
+
+  it("Large tablet", () => {
+    // I know macbook is not a tablet, but it was the closest preset I could find
+    cy.viewport("macbook-11");
+    cy.visit("http://localhost:8080");
+
+    cy.contains("Quem Somos").click();
+    cy.url().should("include", "#about");
+    cy.get("#about").isInViewport();
+
+    cy.contains("Serviços").click();
+    cy.url().should("include", "#services");
+    cy.get("#services").isInViewport();
+
+    cy.contains("Tecnologias").click();
+    cy.url().should("include", "#tecnologies");
+    cy.get("#tecnologies").isInViewport();
+
+    cy.contains("Portfólio").click();
+    cy.url().should("include", "#portfolio");
+    cy.get("#portfolio").isInViewport();
+
+    cy.contains("Solicite Orçamento").click();
+    cy.url().should("include", "#budget");
+    cy.get("#budget").isInViewport();
+
+    cy.get("button").should("not.be.visible");
   });
 });
