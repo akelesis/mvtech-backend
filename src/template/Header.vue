bfe5939bed2764202e1dd29f93fbfdda4bc4c3b2
brenu <bvsousa64@gmail.com>
2021-02-10 21:47:22 -0300

    :art: Make minor adjustments in Header template

--- src/template/Header.vue
+++ src/template/Header.vue
@@ -1,9 +1,26 @@
 <template>
-  <div class="header-cotainer"></div>
+  <div class="header-cotainer">
+    <Section :color="'#101838'">
+      <img :src="companyLogo" alt="Logo da empresa" id="mvtech-logo" />
+    </Section>
+    <Menu />
+  </div>
 </template>
 
 <script>
-export default {};
+import companyLogo from "./assets/images/mvtech.png";
+import Section from "../components/Section";
+
+export default {
+  data() {
+    return {
+      companyLogo,
+    };
+  },
+  components: {
+    Section,
+  },
+};
 </script>
 
 <style scoped>
