6da4a76a97895192d607bdd8bc743ae81d09f125
akelesis <marcus.tavares@outlook.com>
2021-03-02 02:28:26 -0300

    fix port config

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const app = require("express")();
 const consign = require("consign");
 
+const PORT = process.env.PORT || 5000
+
 consign()
     .include("./config/middleware.js")
     .then("./api")
     .then("./config/routes.js")
     .into(app);
 
-app.listen(5000, () =>{
+app.listen(PORT, () =>{
     console.log("Backend executando ╰(*°▽°*)╯ -> 5000");
 })
